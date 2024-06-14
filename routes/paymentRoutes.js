868c0b45b5a3171117e6946b313d4dd25cac5d7c
DikyAhmad <diky.ahmad28@gmail.com>
2024-06-14 15:32:20 +0700

    cleaning routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -7,8 +7,5 @@ const checkApiKey = require('../middleware/checkApiKey');
 // Suggestion API
 router.get('/order-status/:orderId', checkApiKey, paymentController.getOrderStatus);
 router.post('/create-transaction', checkApiKey, paymentController.createTransaction);
-router.post('/finish_payment', checkApiKey, paymentController.paymentFinish);
-router.post('/finish', checkApiKey, paymentController.paymentFinish);
-
 
 module.exports = router;
--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const resourceController = require('../controllers/resourceController');
+const paymentController = require('../controllers/paymentController');
 const machineLearningController = require('../controllers/machineLearningController')
 const checkApiKey = require('../middleware/checkApiKey');
 
 // Main API
-router.post('/user-register', checkApiKey, resourceController.registerUser);
-router.get('/user-detail/:email', checkApiKey, resourceController.userDetail);
-router.get('/seller/:id', checkApiKey, resourceController.sellerDetail);
-router.get('/product/:id', checkApiKey, resourceController.productDetail);
+router.post('/user-register', checkApiKey, resourceController.postRegisterUser);
+router.get('/user-detail/:email', checkApiKey, resourceController.getUserDetail);
+router.get('/seller/:id', checkApiKey, resourceController.getSellerDetail);
+router.get('/product/:id', checkApiKey, resourceController.getProductDetail);
 router.get('/order-product-list/', checkApiKey, resourceController.getOrderProductList);
 router.get('/rating/:id', checkApiKey, resourceController.getRatingByProductId);
-router.get('/active-order/:id', checkApiKey, resourceController.activeOrderById);
+router.get('/active-order/:id', checkApiKey, resourceController.getActiveOrderById);
 router.post('/estimate-order', checkApiKey, resourceController.postOrderEstimate);
+router.post('/order', checkApiKey, paymentController.postOrder);
+router.get('/order/:id', checkApiKey, paymentController.getOrder);
+router.get('/product-seller/:id', checkApiKey, resourceController.getProductBySellerId);
+
+// ML API
+router.get('/search', checkApiKey, machineLearningController.getSearch);
+router.get('/search-bar', checkApiKey, machineLearningController.searchBar);
+router.get('/search-category', checkApiKey, machineLearningController.searchByCategory);
 
 // Suggestion API
 router.post('/product', checkApiKey, resourceController.addProduct);
 router.post('/seller', checkApiKey, resourceController.addSeller);
 router.get('/seller-product/:id', checkApiKey, resourceController.getAllSellerProduct);
 router.get('/product-rating/:rating', checkApiKey, resourceController.getProductByRating);
-
-// ML API
-router.get('/search', checkApiKey, machineLearningController.afterSearch);
-router.get('/search-bar', checkApiKey, machineLearningController.searchBar);
-router.get('/search-category', checkApiKey, machineLearningController.searchByCategory);
-
 
 module.exports = router;
