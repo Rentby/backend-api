47e1536c70747b40c6fc71ef7a407264cc6e3b2a
DikyAhmad <diky.ahmad28@gmail.com>
2024-06-16 13:25:12 +0700

    update paymentRoutes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,10 +5,9 @@ const checkApiKey = require('../middleware/checkApiKey');
 
 
 // Payment API
-router.get('/order-status/:orderId', checkApiKey, paymentController.getOrderStatus);
 router.post('/create-transaction', checkApiKey, paymentController.createTransaction);
 router.post('/order', checkApiKey, paymentController.postOrder);
-router.get('/order/:id', checkApiKey, paymentController.getOrder);
-router.get('/booked-date/:id', checkApiKey, paymentController.getBookedDate);
+
+
 
 module.exports = router;
